0fbfa04c5b7748cda2172b917c95bf53ae347cea
马天行 <matianxing4616@100.me>
2023-07-20 22:24:27 +0800

    update

--- wxcloudrun/user/views.py
+++ wxcloudrun/user/views.py
@@ -1,7 +1,7 @@
 import config
 import requests
 from flask import blueprints, request
-from wxcloudrun.response import make_succ_response
+from wxcloudrun.response import make_succ_response, make_err_response
 
 user_api = blueprints.Blueprint('user_api', __name__)
 
@@ -9,11 +9,13 @@ user_api = blueprints.Blueprint('user_api', __name__)
 @user_api.route('/code2session', methods=['GET'])
 def code2session():
     js_code = request.args.get('code')
-    print(config.appid, config.secret, js_code)
     resp = requests.get('https://api.weixin.qq.com/sns/jscode2session', params={
         'grant_type': 'authorization_code',
         'appid': config.appid,
         'secret': config.secret,
         'js_code': js_code,
     }).json()
-    return make_succ_response(resp)
+    if resp['code'] == 0:
+        return make_succ_response(resp['data'])
+    else:
+        return make_err_response(resp['data']['errmsg'])
