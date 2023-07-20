8eb4b8bc2d12fdeef02d8a3db420e6ef26f21ba0
马天行 <matianxing4616@100.me>
2023-07-20 21:53:30 +0800

    update

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from flask import render_template, request
 from run import app
-from user.views import user_api
+from wxcloudrun.user.views import user_api
 
 
 app.register_blueprint(user_api, url_prefix="/user")
